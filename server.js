5d4f49b04e0e96ebb27f5b849d70cae6dcc9a4c5
Dereje1 <Dereje1>
2017-09-22 13:26:00 -0400

    Completed, ready for submit

--- server.js
+++ server.js
@@ -12,10 +12,25 @@ var app = express();
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/:dateVal", function (req, res) {
+  let parsedDateUnix,returnObject
+
+  //if user param is already a number just use number otherwise parse with builtin function
+  parsedDateUnix = Number(req.params.dateVal) ? Number(req.params.dateVal) : Date.parse(req.params.dateVal)
+
+  if(parsedDateUnix){//if valid unix date
+    let naturalDate = new Date(parsedDateUnix)
+    returnObject = {"unix":parsedDateUnix.toString(),"natural":naturalDate.toDateString()}
+  }
+  else{
+    returnObject = {"unix":null,"natural":null}
+  }
+  res.send(returnObject)
 });
 
+app.get("/", function (req, res) {//got this from glitch , basically serves the index
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 // listen for requests :)
 var listener = app.listen(3000, function () {
