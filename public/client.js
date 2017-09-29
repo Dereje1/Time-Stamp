979d5859f3d7d228f8bdb41a7002362927c2e9a8
Dereje1 <Dereje1>
2017-09-28 20:38:01 -0400

    add small input box to make simialr with  the other api projects

--- public/client.js
+++ public/client.js
@@ -1,9 +1,24 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
+$('#urlSubmitForm').submit(function (e) {
+        e.preventDefault();
+        let fullInput = (!$("#usr").val()) ? $("#usr").attr('placeholder') : $("#usr").val()
+        let win = window.open(window.location.href+fullInput, '_blank');
+        win.focus();
+});
 
 $( document ).ready(function() {
-    console.log( "I am ready on the Front End!!" );
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth(); //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+      dd = '0'+dd
+    }
+
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+    today = monthNames[mm] + ' ' + dd + ',' + yyyy;
+    $("#usr").attr('placeholder',today)
+    console.log (today)
 });
